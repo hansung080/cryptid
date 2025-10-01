41c0d338b2d989087a7519c85653102a364ce836
hansung <hansung080@hanmail.net>
2025-10-01 20:38:37 +0900

    added mocking and test_double examples

---
+++ examples/mocking/test_summer.py
@@ -0,0 +1,31 @@
+from unittest import mock
+from unittest.mock import MagicMock
+
+import mod1
+import mod2
+
+
+def test_summer() -> None:
+    assert mod2.summer(5, 6) == "The sum is 11"
+
+
+def test_summer_mock1() -> None:
+    with mock.patch("mod1.preamble", return_value=""):
+        assert mod2.summer(5, 6) == "11"
+
+
+def test_summer_mock2() -> None:
+    with mock.patch("mod1.preamble") as mock_preamble:
+        mock_preamble.return_value = ""
+        assert mod2.summer(5, 6) == "11"
+
+
+@mock.patch("mod1.preamble", return_value="")
+def test_summer_mock3(mock_preamble: MagicMock) -> None:
+    assert mod2.summer(5, 6) == "11"
+
+
+@mock.patch("mod1.preamble")
+def test_summer_mock4(mock_preamble: MagicMock) -> None:
+    mock_preamble.return_value = ""
+    assert mod2.summer(5, 6) == "11"
---
+++ examples/test_double/test_summer.py
@@ -0,0 +1,9 @@
+import os
+
+os.environ["UNIT_TEST"] = "true"
+
+import mod2
+
+
+def test_summer_fake() -> None:
+    assert mod2.summer(5, 6) == "11"
